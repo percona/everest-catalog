201ae2cfe299412f037256691f863278423049ec
Michal Kralik <michal.kralik@percona.com>
2024-05-15 12:23:22 +0200

    More tests

--- tools/veneer-update_test.go
+++ tools/veneer-update_test.go
@@ -7,18 +7,91 @@ import (
 )
 
 func TestPatch(t *testing.T) {
-	b, err := createPatchVersionVeneer("./everest-operator_test.yaml", "fast-v0", "0.9.1", "0.9.2")
-	require.NoError(t, err)
-	require.Equal(t, patchExpected, string(b))
+	t.Parallel()
+
+	testcases := []struct {
+		name           string
+		channel        string
+		currentVersion string
+		newVersion     string
+		expectedOutput string
+	}{
+		{
+			name:           "Stable patch version",
+			channel:        "fast-v0",
+			currentVersion: "0.9.1",
+			newVersion:     "0.9.2",
+			expectedOutput: patchStableExpected,
+		},
+		{
+			name:           "RC patch version over stable",
+			channel:        "fast-v0",
+			currentVersion: "0.9.1",
+			newVersion:     "0.9.2-rc1",
+			expectedOutput: patchStableRCExpected,
+		},
+		{
+			name:           "RC patch version over RC",
+			channel:        "fast-v1",
+			currentVersion: "0.9.1-rc1",
+			newVersion:     "0.9.1-rc2",
+			expectedOutput: patchRCExpected,
+		},
+	}
+
+	for _, tt := range testcases {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			b, err := createPatchVersionVeneer(
+				"./everest-operator_test.yaml", tt.channel, tt.currentVersion, tt.newVersion,
+			)
+			require.NoError(t, err)
+			require.Equal(t, tt.expectedOutput, string(b))
+		})
+	}
 }
 
 func TestMinor(t *testing.T) {
-	b, err := createMinorVersionVeneer("./everest-operator_test.yaml", "fast-v0", "0.9.1", "0.10.0")
-	require.NoError(t, err)
-	require.Equal(t, minorExpected, string(b))
+	t.Parallel()
+
+	testcases := []struct {
+		name           string
+		channel        string
+		currentVersion string
+		newVersion     string
+		expectedOutput string
+	}{
+		{
+			name:           "Stable minor version",
+			channel:        "fast-v0",
+			currentVersion: "0.9.1",
+			newVersion:     "0.10.0",
+			expectedOutput: minorStableExpected,
+		},
+		{
+			name:           "Stable minor RC version",
+			channel:        "fast-v0",
+			currentVersion: "0.9.1",
+			newVersion:     "0.10.0-rc1",
+			expectedOutput: minorStableRCExpected,
+		},
+	}
+
+	for _, tt := range testcases {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			b, err := createMinorVersionVeneer(
+				"./everest-operator_test.yaml", tt.channel, tt.currentVersion, tt.newVersion,
+			)
+			require.NoError(t, err)
+			require.Equal(t, tt.expectedOutput, string(b))
+		})
+	}
 }
 
-var patchExpected = `defaultChannel: stable-v0
+var patchStableExpected = `defaultChannel: stable-v0
 name: everest-operator
 schema: olm.package
 ---
@@ -37,6 +110,18 @@ package: everest-operator
 schema: olm.channel
 ---
 entries:
+    - name: everest-operator.v0.0.0
+    - name: everest-operator.v0.9.0
+    - name: everest-operator.v0.9.1-rc1
+      replaces: everest-operator.v0.0.0
+      skips:
+        - everest-operator.v0.0.0
+        - everest-operator.v0.9.0
+name: fast-v1
+package: everest-operator
+schema: olm.channel
+---
+entries:
     - name: everest-operator.v0.9.0
     - name: everest-operator.v0.9.1
       skips:
@@ -50,7 +135,95 @@ schema: olm.bundle
 ---
 `
 
-var minorExpected = `defaultChannel: stable-v0
+var patchStableRCExpected = `defaultChannel: stable-v0
+name: everest-operator
+schema: olm.package
+---
+entries:
+    - name: everest-operator.v0.0.0
+    - name: everest-operator.v0.9.0
+    - name: everest-operator.v0.9.1
+    - name: everest-operator.v0.9.2-rc1
+      replaces: everest-operator.v0.0.0
+      skips:
+        - everest-operator.v0.0.0
+        - everest-operator.v0.9.0
+        - everest-operator.v0.9.1
+name: fast-v0
+package: everest-operator
+schema: olm.channel
+---
+entries:
+    - name: everest-operator.v0.0.0
+    - name: everest-operator.v0.9.0
+    - name: everest-operator.v0.9.1-rc1
+      replaces: everest-operator.v0.0.0
+      skips:
+        - everest-operator.v0.0.0
+        - everest-operator.v0.9.0
+name: fast-v1
+package: everest-operator
+schema: olm.channel
+---
+entries:
+    - name: everest-operator.v0.9.0
+    - name: everest-operator.v0.9.1
+      skips:
+        - everest-operator.v0.9.0
+name: stable-v0
+package: everest-operator
+schema: olm.channel
+---
+image: docker.io/perconalab/everest-operator-bundle:0.0.0
+schema: olm.bundle
+---
+`
+
+var patchRCExpected = `defaultChannel: stable-v0
+name: everest-operator
+schema: olm.package
+---
+entries:
+    - name: everest-operator.v0.0.0
+    - name: everest-operator.v0.9.0
+    - name: everest-operator.v0.9.1
+      replaces: everest-operator.v0.0.0
+      skips:
+        - everest-operator.v0.0.0
+        - everest-operator.v0.9.0
+name: fast-v0
+package: everest-operator
+schema: olm.channel
+---
+entries:
+    - name: everest-operator.v0.0.0
+    - name: everest-operator.v0.9.0
+    - name: everest-operator.v0.9.1-rc1
+    - name: everest-operator.v0.9.1-rc2
+      replaces: everest-operator.v0.0.0
+      skips:
+        - everest-operator.v0.0.0
+        - everest-operator.v0.9.0
+        - everest-operator.v0.9.1-rc1
+name: fast-v1
+package: everest-operator
+schema: olm.channel
+---
+entries:
+    - name: everest-operator.v0.9.0
+    - name: everest-operator.v0.9.1
+      skips:
+        - everest-operator.v0.9.0
+name: stable-v0
+package: everest-operator
+schema: olm.channel
+---
+image: docker.io/perconalab/everest-operator-bundle:0.0.0
+schema: olm.bundle
+---
+`
+
+var minorStableExpected = `defaultChannel: stable-v0
 name: everest-operator
 schema: olm.package
 ---
@@ -68,6 +241,65 @@ entries:
         - everest-operator.v0.9.0
         - everest-operator.v0.9.1
 name: fast-v0
+package: everest-operator
+schema: olm.channel
+---
+entries:
+    - name: everest-operator.v0.0.0
+    - name: everest-operator.v0.9.0
+    - name: everest-operator.v0.9.1-rc1
+      replaces: everest-operator.v0.0.0
+      skips:
+        - everest-operator.v0.0.0
+        - everest-operator.v0.9.0
+name: fast-v1
+package: everest-operator
+schema: olm.channel
+---
+entries:
+    - name: everest-operator.v0.9.0
+    - name: everest-operator.v0.9.1
+      skips:
+        - everest-operator.v0.9.0
+name: stable-v0
+package: everest-operator
+schema: olm.channel
+---
+image: docker.io/perconalab/everest-operator-bundle:0.0.0
+schema: olm.bundle
+---
+`
+
+var minorStableRCExpected = `defaultChannel: stable-v0
+name: everest-operator
+schema: olm.package
+---
+entries:
+    - name: everest-operator.v0.0.0
+    - name: everest-operator.v0.9.0
+    - name: everest-operator.v0.9.1
+      replaces: everest-operator.v0.0.0
+      skips:
+        - everest-operator.v0.0.0
+        - everest-operator.v0.9.0
+    - name: everest-operator.v0.10.0-rc1
+      replaces: everest-operator.v0.9.0
+      skips:
+        - everest-operator.v0.9.0
+        - everest-operator.v0.9.1
+name: fast-v0
+package: everest-operator
+schema: olm.channel
+---
+entries:
+    - name: everest-operator.v0.0.0
+    - name: everest-operator.v0.9.0
+    - name: everest-operator.v0.9.1-rc1
+      replaces: everest-operator.v0.0.0
+      skips:
+        - everest-operator.v0.0.0
+        - everest-operator.v0.9.0
+name: fast-v1
 package: everest-operator
 schema: olm.channel
 ---
